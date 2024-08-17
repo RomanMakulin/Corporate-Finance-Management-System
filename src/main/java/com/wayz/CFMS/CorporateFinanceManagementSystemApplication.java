1041b17144b50b111a80c0b30ddd0cb186bb562d
Roman Makulin <sup.makulin@mail.ru>
2024-08-17 12:02:08 +0300

    project files injected

---
+++ src/main/java/com/wayz/CFMS/CorporateFinanceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.wayz.CFMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateFinanceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporateFinanceManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wayz/Corporate_Finance_Management_System/CorporateFinanceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wayz.Corporate_Finance_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporateFinanceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
