ef39530a660997a88f330fe8a49e7d07a56c4f32
WayWayz <sup.makulin@mail.ru>
2024-08-21 13:50:48 +0300

    Добавлены новые методы

---
+++ src/main/java/com/wayz/CFMS/services/user/UserManageService.java
@@ -0,0 +1,34 @@
+package com.wayz.CFMS.services.user;
+
+import com.wayz.CFMS.dto.UserRegistrationData;
+import com.wayz.CFMS.models.User;
+import com.wayz.CFMS.models.subModels.UserActivityStatus;
+
+public interface UserManageService {
+
+    /**
+     * Частичное обновления порльзователя собой же
+     *
+     * @param updateData объект с данными для обновления пользователя
+     * @return обновленный объект пользователя
+     */
+    User updateCurrentUser(UserRegistrationData updateData);
+
+    /**
+     * Полное обновление пользователя администратором
+     *
+     * @param updateData объект с данными для обновления пользователя
+     * @return обновленный объект пользователя
+     */
+    User updateUserByAdmin(User updateData);
+
+    /**
+     * Изменить статус активности пользоателю
+     *
+     * @param login      логин пользователя
+     * @param userStatus новый статус пользователя
+     * @return обновленный объект пользователя
+     */
+    User updateUserStatus(String login, UserActivityStatus userStatus);
+
+}
